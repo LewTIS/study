f9cd45c6ed2a7bc3fa3b58a5e3879963c30f1c63
ningkanglv <ningkanglv@msi.com>
2025-04-09 16:25:01 +0800

    Daily update: 2025-04-09 16:25

--- Projects/C8015_UI_TOOL/C801_packing_tools.py
+++ Projects/C8015_UI_TOOL/C801_packing_tools.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from tkinter import ttk
 from tkinter import messagebox
+from tkinter import filedialog
 import hashlib,os
 import zipfile
 
@@ -10,7 +11,7 @@ root.title("C801 EVSE USB FW packing tool")
 root.configure(bg="#f0f2f5")
 
 # set window size and position
-window_width = 450
+window_width = 500
 window_height = 300
 screen_width = root.winfo_screenwidth()
 screen_height = root.winfo_screenheight()
@@ -26,32 +27,37 @@ main_frame.grid(row=0, column=0, sticky=(tk.W, tk.E, tk.N, tk.S))
 title_label = tk.Label(main_frame,text='C801 EVSE USB FW packing tool',
                        font=("Calibri", 14, "bold"),
                        bg="#f0f2f5",
-                       fg="black")
+                       fg="black",)
 #Place the label in the main window
-title_label.grid(row=0, column=0, columnspan=2, pady=15)
+title_label.grid(row=0, column=0, columnspan=3, pady=15)
 
-# set label and combobox width
-LABEL_WIDTH = 12
-COMBOBOX_WIDTH = 25
+# set label and entry width
+LABEL_WIDTH = 15
+ENTRY_WIDTH = 25
 
-#add "CBS FW:" label and "CBS FW" combobox
-
+#add "CBS FW:" label and "CBS FW" entry
 cbs_fw_label = tk.Label(main_frame, 
                         text='CBS FW :', 
                         font=("Arial", 13, 'bold'), 
                         bg="#f0f2f5",
                         width=LABEL_WIDTH, 
                         anchor='w')
+
 cbs_fw_label.grid(row=1, column=0, sticky=tk.W, pady=10, padx=(30,0))
 cbs_fw_var = tk.StringVar()
-cbs_fw_combobox = ttk.Combobox(main_frame, 
-                               textvariable=cbs_fw_var, 
-                               values=["RC8015RMSI_C0030.bmap.gz","option 2","option 3"], 
-                               font=('Arial', 12),
-                               width=COMBOBOX_WIDTH)
-cbs_fw_combobox.grid(row=1, column=1, sticky=tk.W, pady=10, padx=(0,30))
+cbs_entry = ttk.Entry(main_frame, textvariable=cbs_fw_var, font=('Arial', 11), width=ENTRY_WIDTH)
+cbs_entry.grid(row=1, column=1, sticky=tk.W, padx=(0,2))
 
-#add "HMI FW:" label and "HMI FW" combobox
+def select_cbs_file():
+    file_path = filedialog.askopenfilename(
+        title="Select CBS FW file",
+        filetypes=(("BMAP files", "*.bmap.gz"), ("All files", "*.*"))
+    )
+    if file_path:
+        cbs_fw_var.set(file_path)
+ttk.Button(main_frame, text="Browse...", command=select_cbs_file, width=8).grid(row=1, column=2)
+
+#add "HMI FW:" label and "HMI FW" entry
 
 hmi_fw_label = tk.Label(main_frame, 
                         text='HMI FW  :', 
@@ -61,14 +67,20 @@ hmi_fw_label = tk.Label(main_frame,
                         anchor='w')
 hmi_fw_label.grid(row=2, column=0, sticky=tk.W, pady=10, padx=(30,0))
 hmi_fw_var = tk.StringVar()
-hmi_fw_combobox = ttk.Combobox(main_frame, 
-                               textvariable=hmi_fw_var, 
-                               values=["RC8017IMSI_D0020.bin","option 2","option 3"], 
-                               font=('Arial', 12),
-                               width=COMBOBOX_WIDTH)
-hmi_fw_combobox.grid(row=2, column=1, sticky=tk.W, pady=10, padx=(0,30))
+hmi_entry = ttk.Entry(main_frame, textvariable=hmi_fw_var, font=('Arial', 11), width=ENTRY_WIDTH)
+hmi_entry.grid(row=2, column=1, sticky=tk.W, padx=(0,2))
 
-#add "CB FW:" label and "CB FW" combobox
+def select_hmi_file():
+    file_path = filedialog.askopenfilename(
+        title="Select HMI FW file",
+        filetypes=(("BIN files", "*.bin"), ("All files", "*.*"))
+    )
+    if file_path:
+        hmi_fw_var.set(file_path)
+
+ttk.Button(main_frame, text="Browse...", command=select_hmi_file, width=8).grid(row=2, column=2)
+
+#add "CB FW:" label and "CB FW" entry
 cb_fw_label = tk.Label(main_frame, 
                        text='CB FW   :', 
                        font=("Arial", 13, 'bold'), 
@@ -76,14 +88,18 @@ cb_fw_label = tk.Label(main_frame,
                        width=LABEL_WIDTH,
                        anchor='w')
 cb_fw_label.grid(row=3, column=0, sticky=tk.W, pady=10, padx=(30,0))
-
 cb_fw_var = tk.StringVar()
-cb_fw_combobox = ttk.Combobox(main_frame, 
-                              textvariable=cb_fw_var, 
-                              values=["RC8011EMSI_E0040.bin","option 2","option 3"], 
-                              font=('Arial', 12),
-                              width=COMBOBOX_WIDTH)
-cb_fw_combobox.grid(row=3, column=1, sticky=tk.W, pady=10, padx=(0,30))
+cb_entry = ttk.Entry(main_frame, textvariable=cb_fw_var, font=('Arial', 11), width=ENTRY_WIDTH)
+cb_entry.grid(row=3,column=1,sticky=tk.W,padx=(0,2))
+def select_cb_file():
+    file_path = filedialog.askopenfilename(
+        title="Select CB FW file",
+        filetypes=(("BIN files", "*.bin"), ("All files", "*.*"))
+    )
+    if file_path:
+        cb_fw_var.set(file_path) 
+
+ttk.Button(main_frame, text="Browse...", command=select_cb_file, width=8).grid(row=3, column=2)
 
 #add "EVSE version" label and "EVSE version" combobox
 
@@ -99,9 +115,9 @@ evse_version_var = tk.StringVar()
 evse_version_entry = tk.Entry(main_frame, 
                               textvariable=evse_version_var, 
                               font=('Arial', 12),
-                              width=COMBOBOX_WIDTH,
+                              width=ENTRY_WIDTH-3,
                               )
-evse_version_entry.grid(row=4, column=1, pady=10, padx=(5,30))
+evse_version_entry.grid(row=4, column=1, pady=10, padx=(0,2))
 
 
 def generate_config_file():
@@ -110,10 +126,19 @@ def generate_config_file():
     cb_fw_value = cb_fw_var.get()
     evse_version_value = evse_version_var.get() 
     
+    # check if the input item is empty
     if not all([cbs_fw_value,hmi_fw_value,cb_fw_value,evse_version_value]):
         tk.messagebox.showinfo("Warning", "Please fill all fields!")
         return
     
+    #check if the file exists
+    selected_files = [cbs_fw_value, hmi_fw_value, cb_fw_value]
+    for file in selected_files:
+        if not os.path.exists(file):
+            tk.messagebox.showinfo("Warning", f"{file} does not exist!")
+            return
+        
+    #caculate the selected file checksum
     def caculate_md5(file_path):
         if not os.path.exists(file_path):
             return ""
@@ -127,29 +152,35 @@ def generate_config_file():
     hmi_fw_md5 = caculate_md5(hmi_fw_value)
     cb_fw_md5 = caculate_md5(cb_fw_value)
 
-    config_content = f'BMAP_GZ={cbs_fw_value}\n'
+    #content of the config file
+    config_content = f'BMAP_GZ={os.path.basename(cbs_fw_value)}\n'
     config_content += f'BMAP_GZ.md5={cbs_fw_md5}\n'
-    config_content += f'HMIFW={hmi_fw_value}\n'
+    config_content += f'HMIFW={os.path.basename(hmi_fw_value)}\n'
     config_content += f'HMIFW.md5={hmi_fw_md5}\n'
-    config_content += f'CBFW={cb_fw_value}\n'
+    config_content += f'CBFW={os.path.basename(cb_fw_value)}\n'
     config_content += f'CBFW.md5={cb_fw_md5}\n'
     config_content += f'EVSEVER={evse_version_value}\n'
     
-    with open('msiusbp.conf','w') as f:
-        f.write(config_content)
-    
     #packing file    
-    zip_file_name = f'{evse_version_value}.zip'
-    files_to_zip = [cbs_fw_value,hmi_fw_value,cb_fw_value,'msiusbp.conf'] 
-    with zipfile.ZipFile(zip_file_name,'w') as zipf:
-        for file in files_to_zip:
-            if os.path.exists(file):
-                zipf.write(file,os.path.basename(file)) 
-            else:
-                print(f'file {file} not exist!')
+    zip_path = filedialog.asksaveasfilename(
+        title="Save Zip File",
+        initialfile=f"{evse_version_value}.zip",
+        filetypes=(("Zip files", "*.zip"), ("All files", "*.*"))
+    )
+    if not zip_path:
+        return
+
+    try:
+        with zipfile.ZipFile(zip_path,'w') as zipf:
+            for file in [cbs_fw_value,hmi_fw_value,cb_fw_value]:
+                    zipf.write(file,os.path.basename(file)) 
             
-    tk.messagebox.showinfo("Success", "Configuration file generated successfully.")
-    
+            zipf.writestr('msiusbp.conf',config_content)
+                    
+        #notify success message        
+        tk.messagebox.showinfo("Success", f"The file has been saved to:\n{zip_path}")
+    except Exception as e:
+        tk.messagebox.showerror("Error", f"An error occurred: {e}")
 
 generate_button = tk.Button(root,text="Generate",
                             command=generate_config_file,
@@ -159,5 +190,4 @@ generate_button = tk.Button(root,text="Generate",
                             )
 generate_button.grid(row=5, column=0, columnspan=2, pady=5)
 
-root.after(800, lambda: messagebox.showinfo("Tip", "Please put the current program and the selected file in the same directory!"))
 root.mainloop()
